c3ce104ab6a86e05d350e819107426a220363037
zhongyue3 <zhongyue3@staff.weibo.com>
2023-12-12 19:23:18 +0800

    提交第一版

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import App from './App.vue';
+import router from './router';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(createPinia());
+app.use(router);
+
+
+app.mount('#app');
---
+++ src/views/Login.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="login">
+      <h2>Login Page</h2>
+      <form @submit.prevent="login">
+        <label for="username">Username:</label>
+        <input type="text" id="username" v-model="username">
+        <br>
+        <label for="password">Password:</label>
+        <input type="password" id="password" v-model="password">
+        <br>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        username: '',
+        password: '',
+      };
+    },
+    methods: {
+      login() {
+        // 执行登录逻辑
+        // 可以在这里发送登录请求，验证用户名和密码等
+        console.log('Logging in...');
+      },
+    },
+  };
+  </script>
+  
+  <style>
+  .login {
+    margin: auto;
+    width: 300px;
+    padding: 20px;
+    border: 1px solid #ccc;
+  }
+  </style>

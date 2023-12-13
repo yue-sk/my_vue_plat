513feb83fc9f11f752f463e0207b8053963b3d32
zhongyue3 <zhongyue3@staff.weibo.com>
2023-12-13 15:27:04 +0800

    提交第二版

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
-  </div>
+  <el-config-provider :locale="zhCn">
+      <router-view />
+  </el-config-provider>
 </template>
 
-<script>
-export default {
-  name: 'App'
-}
+<script setup lang="ts">
+import { ElConfigProvider } from 'element-plus';
+import zhCn from 'element-plus/es/locale/lang/zh-cn';
 </script>
+<style>
+@import './assets/css/main.css';
+@import './assets/css/color-dark.css';
+</style>

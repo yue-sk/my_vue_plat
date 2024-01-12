fa87eb2e8ddb99993d34abadd5c4c11da534d75f
zhongyue3 <zhongyue3@staff.weibo.com>
2024-01-12 17:31:02 +0800

    后端获取数据 传向前端

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,17 @@
 import request from '../utils/request';
 
+// fetchData 函数的功能是获取本地 JSON 文件的数据，因此我们可以把它看作是从服务器端获取数据的过程。这个函数可以被其他模块引用和调用，以便在应用程序中获取数据并进行其他操作。
 export const fetchData = () => {
     return request({
         url: './table.json',
+        method: 'get'
+    });
+};
+
+//从api中获取
+export const fetchCommits = () => {
+    return request({
+        url:'https://www.fastmock.site/mock/6b6bab14f8dd4040d5d82be2508bca07/yue_api/api/get_data',
         method: 'get'
     });
 };
--- src/views/basedemo.vue
+++ src/views/basedemo.vue
@@ -4,26 +4,41 @@
 
 <script>
 import * as echarts from 'echarts'; // 引入 Echarts
+import { fetchCommits } from '../api/index';
 
 export default {
   name: 'LineChart',
+  data() {
+    return {
+      Xdata: [] // 定义一个用于存放 x 轴数据的变量
+    };
+  },
   mounted() {
-    setTimeout(() => {
-      this.renderChart();
-    }, 200);
+    this.fetchData(); // 在组件挂载时获取数据
   },
   methods: {
+    fetchData() {
+      fetchCommits()
+        .then(response => {
+          console.log(response.data["X"]); // 打印返回的数据
+          this.Xdata = response.data["X"]; // 将返回的数据赋值给 Xdata 变量
+          this.renderChart(); // 调用渲染图表的方法
+        })
+        .catch(error => {
+          console.error(error); // 处理错误
+        });
+    },
     renderChart() {
       const chartDom = this.$refs.chart;
       const chart = echarts.init(chartDom);
-
+      
       const option = {
         tooltip: {
           trigger: 'axis'
         },
         xAxis: {
           type: 'category',
-          data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+          data: this.Xdata // 使用 Xdata 变量作为 x 轴数据
         },
         yAxis: {
           type: 'value'
